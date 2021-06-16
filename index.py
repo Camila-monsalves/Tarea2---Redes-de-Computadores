0a07a5255ad6fe557e54705e0432e9acf48044e3
LuisChity95 <72509928+LuisChity95@users.noreply.github.com>
2021-06-15 22:27:39 -0400

    Create index.py
    prueba index

---
+++ index.py
@@ -0,0 +1,12 @@
+
+import pandas as pd
+
+
+agenda = pd.DataFrame({
+    "Nombre":["Vania","Camila","Luis"]
+    "Teléfono":[133,132,131]
+    "Dirección":["Calle falsa 123","Hualpen","San Pedro"]
+
+}
+)
+agenda
