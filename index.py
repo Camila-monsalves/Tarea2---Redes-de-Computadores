e9daec427c7b02f8d42b7b2156f21c49cd2279eb
LuisChity95 <72509928+LuisChity95@users.noreply.github.com>
2021-06-15 22:48:01 -0400

    Update index.py
    correcion index

--- index.py
+++ index.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 
 agenda = pd.DataFrame({
-    "Nombre":["Vania","Camila","Luis"]
-    "Teléfono":[133,132,131]
+    "Nombre":["Vania","Camila","Luis"],
+    "Teléfono":[133,132,131],
     "Dirección":["Calle falsa 123","Hualpen","San Pedro"]
 
 }
