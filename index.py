fbd3207ae209fbde96d028beec22a13448bc41c3
Camila-monsalves <72463441+Camila-monsalves@users.noreply.github.com>
2021-06-17 10:44:50 -0400

    Lectura de csv
    Lectura de archivo excel, para guardar y leer datos de agenda mediante libreria pandas

--- index.py
+++ index.py
@@ -1,12 +1,13 @@
-
+print("Bienvenido");
 import pandas as pd
 
-
-agenda = pd.DataFrame({
+''' agenda = pd.DataFrame({
     "Nombre":["Vania","Camila","Luis"],
     "Teléfono":[133,132,131],
     "Dirección":["Calle falsa 123","Hualpen","San Pedro"]
 
 }
-)
-agenda
+) '''
+
+agenda = pd.read_csv("agenda_tarea2.csv", sep = ';')
+print(agenda)
