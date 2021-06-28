8bfa1c2acf36f3f4ccc3cec124264f55c33d3111
Camila-monsalves <72463441+Camila-monsalves@users.noreply.github.com>
2021-06-28 01:35:44 -0400

    Ingreso de botones
    Diseño y funcion de botones de la interfaz

--- index.py
+++ index.py
@@ -5,6 +5,7 @@ from tkinter import ttk
 import tkinter as tk
 from tkinter import messagebox as MessageBox
 import csv
+import orden_alfabetico
 
 def no_encontrado(var):
     var_s = str(var)
@@ -27,64 +28,107 @@ def eliminar(NOMBRE_CONTACT):
         else:
             return False
         
+    class App():
+        def __init__(self, root):
+            self.window = root
         
-def limpiar_inbox():
-    inbox_NOMBRE_CONTACT._eliminar(0, 'end')
-    inbox_TELEFONO._eliminar(0, 'end')
-    inbox_DIRECCION._eliminar(0, 'end')
-
-def limpiar_treeview():
-    tree_lista = self.tree.get_children()
-    for item in tree_lista:
-    self.tree._eliminar(item)
-
-def visualizar_csv():
-    contacto = orden_alfabetico.orden_alfabetico()
-    for i, row in enumerate(contacto):
-        NOMBRE_CONTACT = str(row[0])
-        TELEFONO = str(row[1])
-        DIRECCION = str(row[2])
-        self.tree.insert("", 0, text = NOMBRE_CONTACT, values = (TELEFONO, DIRECCION))
+            inbox_frame = LabelFrame(self.window)
+            inbox_frame.grid(row = 0, column = 0)
+         
+            # ---------------- Ventana del nombre, telefono, dirección-----------------
+            inbox_frame = LabelFrame(self.window)
+            inbox_frame.grid(row = 1, column = 0)
+         
+            inbox1_frame = LabelFrame(self.window)
+            inbox1_frame.grid(row = 1, column = 1)
+       
+         # ---------------- Ventana de la agenda-----------------
+            three_frame = LabelFrame(self.window)
+            three_frame.grid(row = 1, column = 1)
         
-def _guardar(NOMBRE_CONTACT, TELEFONO, DIRECCION):
-            s_NOMBRE_CONTACT = NOMBRE_CONTACT
-            s_TELEFONO = TELEFONO
-            s_DIRECCION = DIRECCION
-            with open('agenda_tarea2.csv', 'a') as f:
-                writer = csv.writer(f, lineterminator ='\r', delimiter=',')
+            three_button_frame = LabelFrame(self.window)
+            three_button_frame.grid(row = 3, column = 0)
+
+            Label(inbox_frame, text = 'INGRESE LOS DATOS DEL\n', font = ("Calibri", "13", "normal")).grid(row = 0, column = 0)
+            Label(inbox_frame, text = 'CONTACTO Y SELECCIONE UNA OPCION\n', font = ("Calibri", "13", "normal")).grid(row = 0, column = 1)
+                       
+            Label(inbox_frame, text = 'NOMBRE_CONTACT', font = ("Calibri", "11", "normal")).grid(row = 2, column = 0)
+            inbox_NOMBRE_CONTACT = Entry(inbox_frame, font = ("Calibri", "11", "normal"), width = 30)
+            inbox_NOMBRE_CONTACT.grid(row = 2, column = 1)
+            inbox_NOMBRE_CONTACT.focus()
+
+            Label(inbox_frame, text = 'TELEFONO', font = ("Calibri", "11", "normal")).grid(row = 4, column = 0)
+            inbox_TELEFONO = Entry(inbox_frame, font = ("Calibri", "11", "normal"), width = 30)
+            inbox_TELEFONO.grid(row = 4, column = 1)
+
+            Label(inbox_frame, text = 'DIRECCION', font = ("Calibri", "11", "normal")).grid(row = 6, column = 0)
+            inbox_DIRECCION = Entry(inbox_frame, font = ("Calibri", "11", "normal"), width = 30)
+            inbox_DIRECCION.grid(row = 6, column = 1)
+            Label(inbox_frame, text = '\n\n', font = ("Calibri", "11", "normal")).grid(row = 7, column = 0)
+        
+            self.tree = ttk.Treeview(three_frame, height = 20, columns = ("DOS","TRES"))
+            self.tree.grid(padx = 5, pady = 5, row = 0, column = 0, columnspan = 1)
+            self.tree.heading("#0", text = 'NOMBRE_CONTACT', anchor = CENTER)
+            self.tree.heading("DOS", text = 'TELEFONO', anchor = CENTER)
+            self.tree.heading("TRES", text = 'DIRECCION', anchor = CENTER)
+            
+            def limpiar_inbox():
+                inbox_NOMBRE_CONTACT._eliminar(0, 'end')
+                inbox_TELEFONO._eliminar(0, 'end')
+                inbox_DIRECCION._eliminar(0, 'end')
+
+            def limpiar_treeview():
+                tree_lista = self.tree.get_children()
+                for item in tree_lista:
+                    self.tree._eliminar(item)
+
+            def visualizar_csv():
+                contacto = orden_alfabetico.orden_alfabetico()
+                for i, row in enumerate(contacto):
+                    NOMBRE_CONTACT = str(row[0])
+                    TELEFONO = str(row[1])
+                    DIRECCION = str(row[2])
+                    self.tree.insert("", 0, text = NOMBRE_CONTACT, values = (TELEFONO, DIRECCION))
+        
+            def _guardar(NOMBRE_CONTACT, TELEFONO, DIRECCION):
+                s_NOMBRE_CONTACT = NOMBRE_CONTACT
+                s_TELEFONO = TELEFONO
+                s_DIRECCION = DIRECCION
+                with open('agenda_tarea2.csv', 'a') as f:
+                    writer = csv.writer(f, lineterminator ='\r', delimiter=',')
                 writer.writerow( (s_NOMBRE_CONTACT, s_TELEFONO,s_DIRECCION) )
 
-def _buscar(var_inbox, posicion):
-            lista = []
-            s_var_inbox = str(var_inbox)
-            var_posicion = int(posicion)
-            with open('agenda_tarea2.csv', 'r') as f:
-                leer = csv.leer(f)
+            def _buscar(var_inbox, posicion):
+                lista = []
+                s_var_inbox = str(var_inbox)
+                var_posicion = int(posicion)
+                with open('agenda_tarea2.csv', 'r') as f:
+                    leer = csv.leer(f)
                 for i, row in enumerate(leer):
                     if s_var_inbox == row[var_posicion]:
                         lista = [row[0], row[1], row[2]]
                         break
                     else:
                         continue
-            return lista
+                return lista
 
-def _check(consulta, var_buscar):
-            lista_consulta = consulta
-            var_buscar = var_buscar
-            if lista_consulta == []:
-                no_encontrado(var_buscar)
-            else:
-                NOMBRE_CONTACT = str(lista_consulta[0])
-                TELEFONO = str(lista_consulta[1])
-                DIRECCION = str(lista_consulta[2])
-                self.tree.insert("", 0, text = NOMBRE_CONTACT, values = (TELEFONO, DIRECCION))
-                self.tree.insert("", 0, text = "Buscar resultado por nombre", values = ("Buscar resultado por telefono", "Buscar resultado por dirección"))
+            def _check(consulta, var_buscar):
+                lista_consulta = consulta
+                var_buscar = var_buscar
+                if lista_consulta == []:
+                    no_encontrado(var_buscar)
+                else:
+                    NOMBRE_CONTACT = str(lista_consulta[0])
+                    TELEFONO = str(lista_consulta[1])
+                    DIRECCION = str(lista_consulta[2])
+                    self.tree.insert("", 0, text = NOMBRE_CONTACT, values = (TELEFONO, DIRECCION))
+                    self.tree.insert("", 0, text = "Buscar resultado por nombre", values = ("Buscar resultado por telefono", "Buscar resultado por dirección"))
          
 
-def _check_1(consulta,var_buscar):
-            modificar = consulta
-            var = var_buscar
-            if modificar == []:
-                no_encontrado(var)
-            else:
-                TopLevelModify(self.window, modificar)
+            def _check_1(consulta,var_buscar):
+                modificar = consulta
+                var = var_buscar
+                if modificar == []:
+                    no_encontrado(var)
+                else:
+                    TopLevelModify(self.window, modificar)
