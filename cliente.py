a5c6e1f674d91de91306afbc5482dbb5325e5244
Camila-monsalves <72463441+Camila-monsalves@users.noreply.github.com>
2021-07-18 01:37:57 -0400

    prueba agenda en cliente

--- cliente.py
+++ cliente.py
@@ -24,40 +24,87 @@ except:
     print ("No se puede conectar con el servidor.", IP, " en el puerto ", PUERTO)
     sys.exit(0)
 
-print ("\nConectado, escriba finalizar() para terminar la conección.\n")
 
 def agenda():
-    contactos = []
-    class contact():
-        NOMBRE_CONTACT  = ""
-        TELEFONO        = ""
-        DIRECCION       = ""
-    while True:
-        print("Bienvenido a la Agenda");
-        print("Presione '1' Si desea agregar un nuevo contacto");
-        print("Presione '2' Si desea visualizar un contacto");
-        print("Presione '3' Si desea eliminar un contacto");
-        seleccion = input("Seleccione una opción de las indicadas: ");
+    print("AGENDA");
+    print("Presione '1' Si desea Agregar un nuevo contacto");
+    print("Presione '2' Si desea Buscar un contacto");
+    print("Presione '3' Si desea Eliminar un contacto");
+    print("Presione '4' Si desea Salir");
+    
+def busqueda():
+    print("Presione '5' Si desea realizar una busqueda por nombre");
+    print("Presione '6' Si desea realizar una busqueda por telefono");
+    print("Presione '7' Si desea realizar una busqueda por direccion");
+    
+def eliminar():
+    print("'Presione 8' Si desea Eliminar por nombre");
+    print("'Presione 9' Si desea Eliminar por número");
 
-        if seleccion == "1" :
-            print("Bienvenido a la seccion 'Agregar Contacto'");
-            contacto                = contact()
-            contacto.NOMBRE_CONTACT = input("Ingrese el nombre al nuevo contacto:         ");
-            contacto.TELEFONO       = input("Ingrese el telefono al nuevo contacto:       ");
-            contacto.DIRECCION      = input("Ingrese la dirección al nuevo contacto:      ");
-            contactos.append(contacto)
-        elif seleccion == "2" :
-            print("Bienvenido a la seccion 'Visualizar Contacto'");
-            with open('agenda_tarea2.csv') as csvfile:
-                reader = csv.DictReader(csvfile)
-                for row in reader:
-                    print(row['NOMBRE_CONTACT'], row['TELEFONO'], row['DIRECCION'])
-        elif seleccion == "3" :
-            print("Bienvenido a la seccion 'Eliminar Contacto'");
+agendaContact     = []
+nombre_contact    = []
+telefono_contact  = []
+direccion_contact = []
+
+opciones       = 0
+opcionBusqueda = 0
+agenda()
+
+while opciones != 4:
+    opciones =  int(input("Ingrese la opcion"))
+    if opciones == 1:
+        print("Ingrese el nombre del contacto");
+        nombre_contact      = input()
+        print("Ingrese el numero de teléfono del contacto");
+        telefono_contact    = input()
+        print("Ingrese la direccion del contacto");
+        direccion_contact   = input()
+        agendaContact.append([nombre_contact, telefono_contact, direccion_contact])
+        print(agenda);
+    elif opciones == 2:
+        busqueda()
+        opcionBusqueda = input("¿Por qué opción desea buscar?\n");
+        if opcionBusqueda == 5: 
+            nombre_contact = input("Nombre contacto buscado:")
+            if nombre_contact in telefono_contact: 
+                print("Telefono:", telefono_contact[nombre_contact]);
+            else:
+                print("La busqueda no arroja nada");
+                          
+        elif opcionBusqueda == 6:
+            telefono_contact = input("Telefono contacto buscado:")
+            if telefono_contact in nombre_contact:
+                print("Nombre:", nombre_contact[telefono_contact]);
+            else:
+                print("La busqueda no arroja nada");
+        
+        elif opcionBusqueda == 7:
+            direccion_contact = input("Direccion del contacto buscado:")
+            if direccion_contact in nombre_contact:
+                print("Nombre:", direccion_contact[nombre_contact]);
+            else:
+                print("La busqueda no arroja nada");
+    elif opciones == 3:
+        eliminar()
+        opcionEliminar = input("")
+        if opcionEliminar == 8:
+            nombre_contact = input("Nombre: ")
+            if nombre_contact not in agenda[0:100]:
+                print('Contacto eliminado con exito');
+            else:
+                print("No existe el nombre a eliminar");
+                
+        elif opcionEliminar == 9:
+            telefono_contact = input("Número: ")
+            if telefono_contact not in agenda[0:100]:
+                print('Contacto eliminado con exito');
+            else:
+                print("No existe el nombre a eliminar");
             
-''' agenda = pd.read_csv("agenda_tarea2.csv", sep = ';')
-print(agenda) '''
-''' agenda()  '''
+            
+            
+
+print ("\nConectado, escriba finalizar() para terminar la conección.\n")
 try:
     while True:
         mensaje = str(input("Yo >> "))
