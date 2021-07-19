57730788c1cecece1a774925f9b71b1b4b6a1328
Camila-monsalves <72463441+Camila-monsalves@users.noreply.github.com>
2021-07-19 05:31:19 -0400

    act cliente

--- cliente.py
+++ cliente.py
@@ -27,150 +27,149 @@ except:
 
 try:
     while True:
-        opciones = str(input("Yo >> "))
-        socket_cliente.send(opciones.encode("utf-8"))
-        if opciones == 4:
-            opciones == 'Adios'
-            socket_cliente.send(opciones.encode())
-            break
-        recibido = socket_cliente.recv(1024).decode('utf-8')
-        print ("Servidor >> " + recibido)
         
-    def agenda():
-        print("AGENDA");
-        print("Presione '1' Si desea Agregar un nuevo contacto");
-        print("Presione '2' Si desea Buscar un contacto");
-        print("Presione '3' Si desea Eliminar un contacto");
-        print("Presione '4' Si desea Salir");
+        def agenda():
+            print("AGENDA");
+            print("Presione '1' Si desea Agregar un nuevo contacto");
+            print("Presione '2' Si desea Buscar un contacto");
+            print("Presione '3' Si desea Eliminar un contacto");
+            print("Presione '4' Si desea Salir");
 
-    def busqueda():
-        print("Presione '5' Si desea realizar una busqueda por nombre");
-        print("Presione '6' Si desea realizar una busqueda por telefono");
-        print("Presione '7' Si desea realizar una busqueda por direccion");
+        def busqueda():
+            print("Presione '5' Si desea realizar una busqueda por nombre");
+            print("Presione '6' Si desea realizar una busqueda por telefono");
+            print("Presione '7' Si desea realizar una busqueda por direccion");
     
-    def eliminar():
-        print("Presione '8' Si desea eliminar por nombre");
-        print("Presione '9' Si desea eliminar por telefono");
-        print("Presione '10' Si desea eliminar por direccion");
+        def eliminar():
+            print("Presione '8' Si desea eliminar por nombre");
+            print("Presione '9' Si desea eliminar por telefono");
+            print("Presione '10' Si desea eliminar por direccion");
     
-    agendaContact     = []
-    nombre_contact    = []
-    telefono_contact  = []
-    direccion_contact = []
-    entradaRemota     = []
-    mensajeServidor   = []
-    mensajeCliente    = []
+        agendaContact     = []
+        nombre_contact    = []
+        telefono_contact  = []
+        direccion_contact = []
+        entradaRemota     = []
+        mensajeServidor   = []
+        mensajeCliente    = []
 
-    opciones       = 0
-    opcionBusqueda = 0
-    opcionEliminar = 0
-    agenda()
+        opciones       = 0
+        opcionBusqueda = 0
+        opcionEliminar = 0
+        agenda()
 
-    while opciones != 4:
-        opciones =  int(input("Ingrese la opcion: "))
-        if opciones == 1:
-            opciones = '1'
-            socket_cliente.send(opciones.enconde())
-            print("Ingrese el nombre del contacto");
-            nombre_contact      = input()
+        while opciones != 4:
+            opciones =  int(input("Ingrese la opcion: "))
+            if opciones == 1:
+                opciones = '1'
+                socket_cliente.send(opciones.enconde())
+                print("Ingrese el nombre del contacto");
+                nombre_contact      = input()
         
-            print("Ingrese el numero de teléfono del contacto");
-            telefono_contact    = input()
+                print("Ingrese el numero de teléfono del contacto");
+                telefono_contact    = input()
       
-            print("Ingrese la direccion del contacto");
-            direccion_contact   = input() 
-            with open('Tarea2-Redes-de-Computadores/agenda.csv', 'a') as f:
+                print("Ingrese la direccion del contacto");
+                direccion_contact   = input() 
+                with open('Tarea2-Redes-de-Computadores/agenda.csv', 'a') as f:
                     writer = csv.writer(f, lineterminator ='\r')
                     writer.writerow( (nombre_contact, telefono_contact, direccion_contact) )
         
         
             
-        elif opciones == 2:
-            opciones = '2'
-            socket_cliente.send(opciones.enconde())
-            busqueda()
-            opcionBusqueda = int(input("¿Por qué opción desea buscar?\n"));
-            if opcionBusqueda == 5: 
-                print("Nombre del contacto: ")
-                nombre_contact = input()
-                with open('Tarea2-Redes-de-Computadores/agenda.csv', 'r') as f:
-                    reader = list(csv.reader(f))
-                    writer = csv.writer(f, lineterminator ='\r')
-                    for i, row in enumerate(reader):
-                        if nombre_contact == row[0]:
-                            print(','.join(row))
+            elif opciones == 2:
+                opciones = '2'
+                socket_cliente.send(opciones.enconde())
+                busqueda()
+                opcionBusqueda = int(input("¿Por qué opción desea buscar?\n"));
+                if opcionBusqueda == 5: 
+                    print("Nombre del contacto: ")
+                    nombre_contact = input()
+                    with open('Tarea2-Redes-de-Computadores/agenda.csv', 'r') as f:
+                        reader = list(csv.reader(f))
+                        writer = csv.writer(f, lineterminator ='\r')
+                        for i, row in enumerate(reader):
+                            if nombre_contact == row[0]:
+                                print(','.join(row))
         
-            elif opcionBusqueda == 6:
-                opcionBusqueda = '6'
-                socket_cliente.send(opcionBusqueda.enconde())
-                print("Número del contacto: ")
-                telefono_contact = input()
-                with open('Tarea2-Redes-de-Computadores/agenda.csv', 'r') as f:
-                    reader = list(csv.reader(f))
-                    writer = csv.writer(f, lineterminator ='\r')
-                    for i, row in enumerate(reader):
-                        if telefono_contact == row[1]:
-                            print(','.join(row))
+                elif opcionBusqueda == 6:
+                    opcionBusqueda = '6'
+                    socket_cliente.send(opcionBusqueda.enconde())
+                    print("Número del contacto: ")
+                    telefono_contact = input()
+                    with open('Tarea2-Redes-de-Computadores/agenda.csv', 'r') as f:
+                        reader = list(csv.reader(f))
+                        writer = csv.writer(f, lineterminator ='\r')
+                        for i, row in enumerate(reader):
+                            if telefono_contact == row[1]:
+                                print(','.join(row))
                 
-            elif opcionBusqueda == 7:
-                opcionBusqueda = '7'
-                socket_cliente.send(opcionBusqueda.enconde())
-                print("Dirección del contacto: ")
-                direccion_contact = input()
-                with open('Tarea2-Redes-de-Computadores/agenda.csv', 'r') as f:
-                    reader = list(csv.reader(f))
-                    writer = csv.writer(f, lineterminator ='\r')
-                    for i, row in enumerate(reader):
-                        if direccion_contact == row[2]:
-                            print(','.join(row))
+                elif opcionBusqueda == 7:
+                    opcionBusqueda = '7'
+                    socket_cliente.send(opcionBusqueda.enconde())
+                    print("Dirección del contacto: ")
+                    direccion_contact = input()
+                    with open('Tarea2-Redes-de-Computadores/agenda.csv', 'r') as f:
+                        reader = list(csv.reader(f))
+                        writer = csv.writer(f, lineterminator ='\r')
+                        for i, row in enumerate(reader):
+                            if direccion_contact == row[2]:
+                                print(','.join(row))
                         
-        elif opciones == 3:
-            opciones = '3'
-            socket_cliente.send(opciones.enconde())
-            eliminar()
-            opcionEliminar = int(input("¿Por qué opción desea eliminar?\n"));
-            if opcionEliminar == 8: 
-                opcionEliminar = '8'
-                socket_cliente.send(opcionEliminar.enconde())
-                print("Nombre del contacto: ")
-                nombre_contact = input()
-                with open('Tarea2-Redes-de-Computadores/agenda.csv', 'r') as f:
-                    eader = list(csv.reader(f))
-                with open('Tarea2-Redes-de-Computadores/agenda.csv', 'w') as f:
-                    writer = csv.writer(f, lineterminator ='\r')
-                    for i, row in enumerate(reader):
-                        if nombre_contact != row[0]:
-                            writer.writerow(row)
-                print("contacto eliminado")
+            elif opciones == 3:
+                opciones = '3'
+                socket_cliente.send(opciones.enconde())
+                eliminar()
+                opcionEliminar = int(input("¿Por qué opción desea eliminar?\n"));
+                if opcionEliminar == 8: 
+                    opcionEliminar = '8'
+                    socket_cliente.send(opcionEliminar.enconde())
+                    print("Nombre del contacto: ")
+                    nombre_contact = input()
+                    with open('Tarea2-Redes-de-Computadores/agenda.csv', 'r') as f:
+                        eader = list(csv.reader(f))
+                    with open('Tarea2-Redes-de-Computadores/agenda.csv', 'w') as f:
+                        writer = csv.writer(f, lineterminator ='\r')
+                        for i, row in enumerate(reader):
+                            if nombre_contact != row[0]:
+                                writer.writerow(row)
+                    print("contacto eliminado")
             
-            elif opcionEliminar == 9: 
-                opcionEliminar = '9'
-                socket_cliente.send(opcionEliminar.enconde())
-                print("Telefono del contacto: ")
-                telefono_contact = input()
-                with open('Tarea2-Redes-de-Computadores/agenda.csv', 'r') as f:
-                    reader = list(csv.reader(f))
-                with open('Tarea2-Redes-de-Computadores/agenda.csv', 'w') as f:
-                    writer = csv.writer(f, lineterminator ='\r')
-                    for i, row in enumerate(reader):
-                        if telefono_contact != row[1]:
-                            writer.writerow(row)
-                print("contacto eliminado")
+                elif opcionEliminar == 9: 
+                    opcionEliminar = '9'
+                    socket_cliente.send(opcionEliminar.enconde())
+                    print("Telefono del contacto: ")
+                    telefono_contact = input()
+                    with open('Tarea2-Redes-de-Computadores/agenda.csv', 'r') as f:
+                        reader = list(csv.reader(f))
+                    with open('Tarea2-Redes-de-Computadores/agenda.csv', 'w') as f:
+                        writer = csv.writer(f, lineterminator ='\r')
+                        for i, row in enumerate(reader):
+                            if telefono_contact != row[1]:
+                                writer.writerow(row)
+                    print("contacto eliminado")
             
-            elif opcionEliminar == 10: 
-                opcionEliminar = '10'
-                socket_cliente.send(opcionEliminar.enconde())
-                print("Dirección del contacto: ")
-                direccion_contact = input()
-                with open('Tarea2-Redes-de-Computadores/agenda.csv', 'r') as f:
-                    reader = list(csv.reader(f))
-                with open('Tarea2-Redes-de-Computadores/agenda.csv', 'w') as f:
-                    writer = csv.writer(f, lineterminator ='\r')
-                    for i, row in enumerate(reader):
-                        if direccion_contact != row[2]:
-                            writer.writerow(row)
-                print("contacto eliminado")
+                elif opcionEliminar == 10: 
+                    opcionEliminar = '10'
+                    socket_cliente.send(opcionEliminar.enconde())
+                    print("Dirección del contacto: ")
+                    direccion_contact = input()
+                    with open('Tarea2-Redes-de-Computadores/agenda.csv', 'r') as f:
+                        reader = list(csv.reader(f))
+                    with open('Tarea2-Redes-de-Computadores/agenda.csv', 'w') as f:
+                        writer = csv.writer(f, lineterminator ='\r')
+                        for i, row in enumerate(reader):
+                            if direccion_contact != row[2]:
+                                writer.writerow(row)
+                    print("contacto eliminado")
             
+                    socket_cliente.send(opciones.encode("utf-8"))
+                elif opciones == 4:
+                    opciones == 'Adios'
+                    socket_cliente.send(opciones.encode())
+                    break
+            recibido = socket_cliente.recv(1024).decode('utf-8')
+            print ("Servidor >> " + recibido)
 except socket.error:
     print ("Se perdio la conexion con el servidor.")
 except KeyboardInterrupt:
