64087a74a193144478f4aa968dd9dc0a7860d5ce
Vania-Espinoza <72463438+Vania-Espinoza@users.noreply.github.com>
2021-07-18 22:45:49 -0400

    ingresar contacto
    la prueba es para ejecutar en el visual

--- prueba.py
+++ prueba.py
@@ -1,28 +1,4 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-# Programa Cliente
-
-import socket
-import sys
-import pandas as pd
 import csv
-
-        
-if len(sys.argv) != 3:
-    print ("Agregar la IP del servidor y el puerto donde se ofrece el servicio.")
-    sys.exit(0)
-
-IP = sys.argv[1]
-PUERTO = int(sys.argv[2])
-
-print ("\nConectandose al servidor ", IP, " en el puerto ", PUERTO, " ...")
-
-try:
-    socket_cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    socket_cliente.connect((IP, PUERTO))
-except:
-    print ("No se puede conectar con el servidor.", IP, " en el puerto ", PUERTO)
-    sys.exit(0)
 
 
 def agenda():
@@ -41,6 +17,7 @@ def eliminar():
     print("'Presione 8' Si desea Eliminar por nombre");
     print("'Presione 9' Si desea Eliminar por número");
 
+
 agendaContact     = []
 nombre_contact    = []
 telefono_contact  = []
@@ -51,17 +28,22 @@ opcionBusqueda = 0
 agenda()
 
 while opciones != 4:
-    opciones =  int(input("Ingrese la opcion"))
+    opciones =  int(input("Ingrese la opcion: "))
     if opciones == 1:
         print("Ingrese el nombre del contacto");
         nombre_contact      = input()
+        
         print("Ingrese el numero de teléfono del contacto");
         telefono_contact    = input()
+      
         print("Ingrese la direccion del contacto");
-        direccion_contact   = input()
+        direccion_contact   = input() 
         with open('agenda.csv', 'a') as f:
                 writer = csv.writer(f, lineterminator ='\r')
                 writer.writerow( (nombre_contact, telefono_contact, direccion_contact) )
+        
+        
+            
     elif opciones == 2:
         busqueda()
         opcionBusqueda = input("¿Por qué opción desea buscar?\n");
@@ -101,26 +83,4 @@ while opciones != 4:
                 print('Contacto eliminado con exito');
             else:
                 print("No existe el nombre a eliminar");
-            
-            
-            
-
-print ("\nConectado, escriba finalizar() para terminar la conección.\n")
-try:
-    while True:
-        mensaje = str(input("Yo >> "))
-        socket_cliente.send(mensaje.encode("utf-8"))
-        if mensaje == "finalizar()":
-            break
-        recibido = socket_cliente.recv(1024).decode('utf-8')
-        print ("Servidor >> " + recibido)
-
-except socket.error:
-    print ("Se perdio la conexion con el servidor.")
-except KeyboardInterrupt:
-    print ("\nSe interrunpio el cliente con un Control_C.")
-
-finally:
-    print ("Terminando conexion con el servidor ...")
-    socket_cliente.close()
-    print ("Conexion con el servidor terminado.")
+                
